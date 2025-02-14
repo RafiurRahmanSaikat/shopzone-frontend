d58c075b46f3aa091ab8ffd5a258065d79727138
RafiurRahmanSaikat <md.rafiur.rahman.saikat@gmail.com>
2025-02-14 21:36:53 +0600

    Backend Urls Changed

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,7 +2,7 @@ export const backEndApi = "http://127.0.0.1:8000";
 
 // export const BASE_URL_BACKEND = "http://127.0.0.1:8000/api";
 
-export const BASE_URL_BACKEND = "https://shopzone-backend-beta.vercel.app/api";
+export const BASE_URL_BACKEND = "https://shopzone-backend-gilt.vercel.app/api";
 
 export const sidebarLinks = {
   admin: [
